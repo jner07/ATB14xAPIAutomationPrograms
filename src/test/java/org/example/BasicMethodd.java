f6b56a2b5b8a9cd9866f65a3678e555e355478b0
jner07 <jayeshner0705@gmail.com>
2025-12-06 22:03:10 +0530

    API automation

--- src/test/java/org/example/BasicMethodd.java
+++ src/test/java/org/example/BasicMethodd.java
@@ -2,7 +2,7 @@ package org.example;
 
 import io.restassured.RestAssured;
 
-public class BasicMethod {
+public class BasicMethodd {
     public static void main(String[] args) {
         RestAssured.
                 given().baseUri("https://google.com").log().all().
